6c40babaf6c4a8df289d1121f4a9d6ea0fbe9e81
GsxDavid <jmanceraram@gmail.com>
2025-03-05 02:38:01 -0500

    feat(server): agregar express static, middleware de 404 y ruta de inicio

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const path = require('path');
 const db = require('./models');
 const routes = require('./src/routes')
 const swaggerDocs = require('./swaggerConfig');
@@ -9,11 +10,27 @@ const app = express();
 
 app.use(express.json());
 
-// Rutas
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Página de inicio
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Rutas API
 app.use('/api', routes);
+
+
 
 // Cargar swagger
 swaggerDocs(app);
+
+// Middleware de 404
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 
 // Sincronizar la base de datos y arrancar el servidor
 db.sequelize.sync().then(() => {
