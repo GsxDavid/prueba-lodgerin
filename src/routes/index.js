3f7687ed9221b594ec04995ad049160fca361f52
GsxDavid <jmanceraram@gmail.com>
2025-03-04 23:14:16 -0500

    feat: agregar router de autenticación

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const adminRoutes = require('./admin.routes');
 const guestRoutes = require('./guest.routes');
+const authRoutes = require('./auth.routes');
 
 
 router.use('/admin', adminRoutes);
 router.use('/guest', guestRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
